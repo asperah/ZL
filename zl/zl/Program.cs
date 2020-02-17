1688215ada55a7b9fe4bac76b005ed34f1343c7a
asperah <54786409+asperah@users.noreply.github.com>
2020-02-17 15:52:59 +0100

    Add files via upload

---
+++ zl/zl/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool progrun = true;
+            int wahl;
+            int kforwards = 3;
+            int kbackwards = 19;
+            Zirkulator zl = new Zirkulator();
+            while (progrun)
+            {
+                zl.ShowValues();
+                Console.WriteLine("Möchtest du Vorwärts (1) oder Rückwärts (2) gehen? Oder möchtest du das Programm beenden? (3)");
+                wahl = Convert.ToInt32(Console.ReadLine());
+                if (wahl == 1)
+                {
+                    Console.WriteLine("Wie viele Schritte möchtest du Vorwärts gehen?");
+                    kforwards=Convert.ToInt32(Console.ReadLine());
+                    zl.current = zl.SkipXForward(zl.current, kforwards);
+                    Console.WriteLine("Neue Position: " + zl.current.Value);
+                }
+                if (wahl == 3)
+                {
+                    progrun = false;
+                }
+                if (wahl == 2)
+                {
+                    Console.WriteLine("Wie viele Schritte möchtest du Rückwärts gehen?");
+                    kbackwards = Convert.ToInt32(Console.ReadLine());
+                    zl.current = zl.SkipXBackward(zl.current, kbackwards);
+                    Console.WriteLine("Neue Position: " + zl.current.Value);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ zl/zl/Zirkulator.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace zl
+{
+    class Zirkulator
+    {
+        private Entry first, last, newValue;
+        public Entry current;
+       
+        // Konstanten = unveränderliche Werte!
+        private const int standardGroesse = 26;
+        private const int beginnAsCIIBuchstaben = 65;
+
+        /*** Der Konstruktor soll eine zirkuläre, doppelverkettete Liste
+         * mit Einträgen des Alphabets bereitstellen
+         Die Liste soll in beide Richtungen durchlaufen werden!
+         ***/
+        public Zirkulator()
+        {
+            if (first == null)
+            {
+                newValue = new Entry() { Value = 'A' };
+                first = newValue;
+                last = newValue;
+                current = newValue;
+                newValue.Next = first;
+                newValue.Prev = first;
+                for (int i = 1; i < standardGroesse; i++)
+                {
+                        char c = (char)(i + beginnAsCIIBuchstaben);
+                        AddElement(c, last,i);                   
+                }                
+            }
+            else
+            {
+                Console.WriteLine("Da ist schon ne Liste. Du brauchst nur eine.");
+            }
+        }
+
+        // Soll ein Zeichen entgegennehmen und ein Entry in die Liste einfügen
+        private void AddElement(char c, Entry last, int i)
+        {
+            if (i == standardGroesse-1)
+            {
+                newValue = new Entry() { Value = c };
+                last.Next = newValue;
+                this.last = newValue;
+                newValue.Prev = last;
+                newValue.Next = first;
+                first.Prev = newValue;
+            }
+            else
+            {
+            newValue = new Entry() { Value = c };
+            last.Next = newValue;
+            this.last = newValue;
+            newValue.Prev = last;
+            }
+
+        }
+
+
+        // gibt innerhalb der Klasse ein Entry des gesuchten Elements zurück
+        private Entry FindEntry(char c)
+        {
+            return null;
+        }
+
+        // geht von einem gesuchten Entry x Eintragungen in Richtung Nächster
+        // und liefert den Entry
+        public Entry SkipXForward(Entry e, int distance)
+        {
+            
+            for (int i = 0; i < distance; i++)
+            {
+                e = e.Next;
+            }
+            return e;
+        }
+
+
+        // geht von einem gesuchten Entry x Eintragungen in Richtung Vorgänger
+        // und liefert den Entry
+        public Entry SkipXBackward(Entry e, int distance)
+        {
+
+            for (int i = 0; i < distance; i++)
+            {
+               e = e.Prev;
+            }
+            return e;
+        }
+
+
+        // Soll einmal alles anzeigen ...
+        public void ShowValues()
+        {
+                Console.WriteLine("Aktueller Wert: "+current.Value);          
+        }
+
+
+        // inner class = eine Klasse in einer Klasse
+        public class Entry
+        {
+            public char Value { get; set; }
+            public Entry Next { get; set; }
+            public Entry Prev { get; set; }
+        }  // ende Entry
+    }
+}
